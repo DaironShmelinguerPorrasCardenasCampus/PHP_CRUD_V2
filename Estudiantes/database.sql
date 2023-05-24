db1280407a4c6eea1ea6266551a4f6d905622b0e
DaironShmelinguerPorrasCardenasCampus <porras.n123456789@gmail.com>
2023-05-24 08:10:41 -0500

    feat: :memo: CREACIÓN BASE DE DATOS

--- Estudiantes/database.sql
+++ Estudiantes/database.sql
@@ -1,10 +1,15 @@
-CREATE DATABASE campus;
+CREATE DATABASE campusV2;
 
 CREATE TABLE campers(
 
     id INT primary key AUTO_INCREMENT,
     nombres VARCHAR(50) NOT NULL,
     direccion VARCHAR(50) NOT NULL,
-    logros VARCHAR(60)
+    logros VARCHAR(60) NOT NULL,
+    skills FLOAT(50) NOT NULL,
+    review FLOAT(50) NOT NULL,
+    ser FLOAT(50) NOT NULL, 
+    ingles FLOAT(50) NOT NULL,
+    especialidad VARCHAR(50) NOT NULL
 
 );
