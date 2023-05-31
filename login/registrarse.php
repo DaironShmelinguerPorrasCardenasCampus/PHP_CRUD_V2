aa742f8919a3a509604d1b8fa5924659ee538bbe
DaironShmelinguerPorrasCardenasCampus <porras.n123456789@gmail.com>
2023-05-31 06:28:45 -0500

    feat: :memo: INCORPORACIÓN DEL HOME

--- login/registrarse.php
+++ login/registrarse.php
@@ -5,7 +5,7 @@ if(isset($_POST['registrarse'])){
     require_once("RegistroUser.php");
     $registrar = new RegistrarUser();
 
-    $registrar->setIdCamper(2);
+    $registrar->setIdCamper(9);
     $registrar->setEmail($_POST['email']);
     $registrar->setUsername($_POST['username']);
     $registrar->setPassword($_POST['password']);
